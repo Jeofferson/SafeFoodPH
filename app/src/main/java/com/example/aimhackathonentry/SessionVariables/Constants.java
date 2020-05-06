fe7a06a8be31400837e3e170db7730e1ff66acd4
Jeofferson Dela Pena <jeoffersond@gmail.com>
2020-05-06 14:52:53 +0800

    added 10 dummy product data

--- app/src/main/java/com/example/aimhackathonentry/SessionVariables/Constants.java
+++ app/src/main/java/com/example/aimhackathonentry/SessionVariables/Constants.java
@@ -12,5 +12,21 @@ public class Constants {
     public static final int SPLASH_SCREEN_DURATION = 2000;
     public static final int ANIMATION_DURATION = 2000;
 
+    public static final String FRUITS_AND_VEGETABLES = "Fruits and Vegetables";
+
+    public static final String FISH_AND_MARINE_PRODUCTS = "Fish and Marine Products";
+
+    public static final String MEAT_AND_POULTRY_PRODUCTS = "Meat and Poultry Products";
+
+    public static final String FLOUR_AND_BAKERY_PRODUCTS = "Flour and Bakery Products";
+
+    public static final String BEVERAGE_CONFECTIONERY_AND_DAIRY_FOODS = "Beverage, Confectionery, and Dairy Foods";
+
+    public static final String FOOD_CONDIMENTS_AND_SEASONINGS = "Food Condiments and Seasonings";
+
+    public static final String FOOD_SUPPLEMENT_BOTTLED_WATER_AND_SNACK_FOOD = "Food Supplement, Bottled Water, and Snack Food";
+
+    public static final String FATS_AND_OILS = "Fats and Oils";
+
 
 }
